89e6cb17e5fd2f6be20b7fa859a1a0fc04f7f2e0
Yuzu <yuzu.chen@mars-deg.com>
2024-12-16 10:20:53 +0800

    顏色應用上了

--- app.vue
+++ app.vue
@@ -6,7 +6,14 @@
 		<q-btn color="secondary-01" label="secondary-01" />
 		<q-btn color="grayscale-01" label="grayscale-01" />
 		<q-btn color="grayscale-02" label="grayscale-02" />
+		<div class="a">aaaa</div>
 		<!-- <NuxtRouteAnnouncer />
 		<NuxtWelcome /> -->
 	</div>
 </template>
+
+<style lang="scss" scoped>
+.a {
+	color: $grayscale-02;
+}
+</style>
